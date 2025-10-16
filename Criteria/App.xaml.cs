2054592586688e31fb4326739ba596a333d5b285
20114122 <20114122@tafe.wa.edu.au>
2025-10-16 14:03:09 +0800

    feat: complete LoadingPage.xaml

--- Criteria/App.xaml.cs
+++ Criteria/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Pages.FirstBoot.FilmSelectionPage());
+            MainPage = new NavigationPage(new Pages.FirstBoot.LoadingPage());
         }
     }
 }
