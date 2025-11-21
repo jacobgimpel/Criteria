5bed42558a5eb63c32839ef7afa9b5599e581ef1
20114122 <20114122@tafe.wa.edu.au>
2025-11-21 20:14:03 +0800

    feat: add landscape support for RecommendationView

--- Criteria/Pages/MainApp/RecommendationView.xaml.cs
+++ Criteria/Pages/MainApp/RecommendationView.xaml.cs
@@ -1,5 +1,7 @@
 using Criteria.Models;
+using System;
 using System.Collections.Generic;
+using Microsoft.Maui.Controls;
 
 namespace Criteria.Pages.MainApp;
 
@@ -13,6 +15,15 @@ public partial class RecommendationView : ContentPage
         _recommendedMovies = recommendedMovies ?? new List<Movie>();
 
         RecommendationsCarousel.ItemsSource = _recommendedMovies;
+        LandscapeCarousel.ItemsSource = _recommendedMovies;
+
+        RecommendationsCarousel.PositionChanged += (s, e) =>
+        {
+            if (RecommendationsCarousel.CurrentItem is Movie movie)
+            {
+                LandscapeSynopsis.Text = movie.Overview;
+            }
+        };
     }
 
     private async void OnPosterTapped(object sender, TappedEventArgs e)
@@ -46,20 +57,31 @@ public partial class RecommendationView : ContentPage
 
     private void OnSaveButtonClicked(object sender, EventArgs e)
     {
-        if (RecommendationsCarousel.CurrentItem is Movie currentMovie)
+        Movie currentMovie = null;
+
+        if (LandscapeGrid.IsVisible && LandscapeCarousel.CurrentItem is Movie landscapeMovie)
         {
-            bool alreadyAdded = Criteria.Models.SavedFilms.SavedMovies.Any(m => m.TMDBId == currentMovie.TMDBId);
-            if (alreadyAdded)
-            {
-                DisplayAlert("Already Saved", $"{currentMovie.Title} is already saved.", "OK");
-            }
-            else
-            {
-                Criteria.Models.SavedFilms.AddMovie(currentMovie);
-                DisplayAlert("Saved", $"{currentMovie.Title} has been added to your bookmarks.", "OK");
-            }
+            currentMovie = landscapeMovie;
+        }
+        else if (RecommendationsCarousel.CurrentItem is Movie portraitMovie)
+        {
+            currentMovie = portraitMovie;
+        }
+
+        if (currentMovie == null) return;
+
+        bool alreadySaved = Models.SavedFilms.SavedMovies.Exists(m => m.TMDBId == currentMovie.TMDBId);
+        if (alreadySaved)
+        {
+            DisplayAlert("Already Saved", $"{currentMovie.Title} is already saved.", "OK");
+        }
+        else
+        {
+            Models.SavedFilms.AddMovie(currentMovie);
+            DisplayAlert("Saved", $"{currentMovie.Title} has been added to your bookmarks.", "OK");
         }
     }
+
 
     private async void OnHomeClicked(object sender, EventArgs e)
     {
@@ -69,5 +91,38 @@ public partial class RecommendationView : ContentPage
     private async void OnSavedFilmsClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SavedFilms());
+    }
+
+    private void RecommendationView_SizeChanged(object sender, EventArgs e)
+    {
+        if (Width > Height)
+        {
+            AppTitle.IsVisible = false;
+            InstructionsStack.IsVisible = false;
+            FooterGrid.IsVisible = false;
+            RecommendationsCarousel.IsVisible = false;
+
+            LandscapeGrid.IsVisible = true;
+
+            if (LandscapeCarousel.CurrentItem is Movie currentMovie)
+                LandscapeSynopsis.Text = currentMovie.Overview;
+        }
+        else
+        {
+            AppTitle.IsVisible = true;
+            InstructionsStack.IsVisible = true;
+            FooterGrid.IsVisible = true;
+            RecommendationsCarousel.IsVisible = true;
+
+            LandscapeGrid.IsVisible = false;
+        }
+    }
+
+    private void LandscapeCarousel_PositionChanged(object sender, PositionChangedEventArgs e)
+    {
+        if (LandscapeCarousel.CurrentItem is Movie currentMovie)
+        {
+            LandscapeSynopsis.Text = currentMovie.Overview;
+        }
     }
 }
--- Criteria/Pages/MainApp/SavedFilms.xaml.cs
+++ Criteria/Pages/MainApp/SavedFilms.xaml.cs
@@ -6,62 +6,62 @@ namespace Criteria.Pages.MainApp;
 public partial class SavedFilms : ContentPage
 {
     private Movie _selectedMovie;
-	public SavedFilms()
-	{
-		InitializeComponent();
+    public SavedFilms()
+    {
+        InitializeComponent();
 
-		SavedFilmsCollection.SelectionChanged += OnMovieSelected;
-		SavedFilmsCollection.ItemsSource = Models.SavedFilms.SavedMovies;
+        SavedFilmsCollection.SelectionChanged += OnMovieSelected;
+        SavedFilmsCollection.ItemsSource = Models.SavedFilms.SavedMovies;
 
-	}
+    }
 
-	private void OnMovieSelected(object sender, SelectionChangedEventArgs e)
-	{
-		_selectedMovie = e.CurrentSelection.FirstOrDefault() as Movie;
-	}
+    private void OnMovieSelected(object sender, SelectionChangedEventArgs e)
+    {
+        _selectedMovie = e.CurrentSelection.FirstOrDefault() as Movie;
+    }
 
-	private async void OnDeleteButtonClicked(object sender, EventArgs e)
-	{
+    private async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
         if (_selectedMovie == null)
-		{
-			await DisplayAlert("No movie selected", "Please select a movie to delete.", "OK");
-			return;
-		}
-		bool confirm = await DisplayAlert("Confirm Deletion", $"Are you sure you want to delete {_selectedMovie.Title} from your saved films?", "Yes", "No");
-		if (confirm)
-		{
-			Models.SavedFilms.SavedMovies.Remove(_selectedMovie);
-			SavedFilmsCollection.ItemsSource = null;
-			SavedFilmsCollection.ItemsSource = Models.SavedFilms.SavedMovies;
-			_selectedMovie = null;
-		}
-            
+        {
+            await DisplayAlert("No movie selected", "Please select a movie to delete.", "OK");
+            return;
+        }
+        bool confirm = await DisplayAlert("Confirm Deletion", $"Are you sure you want to delete {_selectedMovie.Title} from your saved films?", "Yes", "No");
+        if (confirm)
+        {
+            Models.SavedFilms.SavedMovies.Remove(_selectedMovie);
+            SavedFilmsCollection.ItemsSource = null;
+            SavedFilmsCollection.ItemsSource = Models.SavedFilms.SavedMovies;
+            _selectedMovie = null;
+        }
+
     }
 
     private async void OnPosterTapped(object sender, EventArgs e)
-	{
-		if (sender is Image image)
-		{
-			var parentGrid = image.Parent as Grid;
-			if (parentGrid == null)
-				return;
+    {
+        if (sender is Image image)
+        {
+            var parentGrid = image.Parent as Grid;
+            if (parentGrid == null)
+                return;
 
-			var overlay = parentGrid.FindByName<Grid>("Overlay");
-			if (overlay == null)
-				return;
+            var overlay = parentGrid.FindByName<Grid>("Overlay");
+            if (overlay == null)
+                return;
 
-			if (overlay.IsVisible)
-			{
-				await overlay.FadeTo(0, 250);
-				overlay.IsVisible = false;
-			}
-			else 
-			{
-				overlay.IsVisible = true;
-				await overlay.FadeTo(1, 250);
-			}
-		}
-	}
+            if (overlay.IsVisible)
+            {
+                await overlay.FadeTo(0, 250);
+                overlay.IsVisible = false;
+            }
+            else
+            {
+                overlay.IsVisible = true;
+                await overlay.FadeTo(1, 250);
+            }
+        }
+    }
     private async void OnOverlayTapped(object sender, TappedEventArgs e)
     {
         if (sender is Grid overlay)
@@ -71,9 +71,9 @@ public partial class SavedFilms : ContentPage
         }
     }
 
-	private async void OnHomeClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new MainPage());
-	}
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
 
 }
